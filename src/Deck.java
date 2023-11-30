341c5a0a375bbae30616a37fd9f898634326de72
Benjamin Chock <benjamin.chock@menloschool.org>
2023-11-30 11:53:46 -0800

    Completed Card half of Deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> deck;
+    public Deck(String[] ranks, String[] suit, int[] point){
+        deck = new ArrayList<Card>();
+        for (int i = 0; i < suit.length; i++){
+            for (int j = 0; j < ranks.length; j++){
+                  deck.add(new Card(ranks[j],suit[i],point[j]));
+            }
+
+        }
+    }
 }
